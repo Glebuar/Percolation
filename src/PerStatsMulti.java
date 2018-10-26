45eb449b83f20bdb1b4f0a3b1e7de246c2a27074
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2018-10-25 22:08:49 -0700

    Proposed changes

--- src/PerStatsMulti.java
+++ src/PerStatsMulti.java
@@ -6,42 +6,73 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.*;
 
-public class PerStatsMulti implements Callable<Integer> {
+public class PerStatsMulti  {
 
-    private final int n;
-    private static double[] fraction;
+    private double[] fraction;
+    
+    public PerStatsMulti(int n) {
+        fraction = new double[n];
+    }
+    
+    private static class PerWorker implements Callable<Integer> {
+        
+        private final int n;
+        
+        public PerStatsMulti(final int n) {
+            this.n = n;
+        }
 
-    public PerStatsMulti(final int n) {
-        this.n = n;
+        @Override
+        public Integer call() throws Exception {
+         // Percolation p = new Percolation(n);
+            // 4
+            Percolation2 p = new Percolation2(n);
+            while (!p.percolates()) {
+                int a = StdRandom.uniform(n) + 1;
+                int b = StdRandom.uniform(n) + 1;
+                if (!p.isOpen(a, b)) {
+                    p.open(a, b);
+                }
+            }
+            return p.numberOfOpenSites();
+        }
     }
 
-    @Override
-    public Integer call() throws Exception {
-     // Percolation p = new Percolation(n);
-        Percolation2 p = new Percolation2(n);
-        while (!p.percolates()) {
-            int a = StdRandom.uniform(n) + 1;
-            int b = StdRandom.uniform(n) + 1;
-            if (!p.isOpen(a, b)) {
-                p.open(a, b);
+    private void calculate() {
+        ExecutorService executor = Executors.newFixedThreadPool(12);
+        ThreadPoolExecutor pool = (ThreadPoolExecutor) executor;
+        List<Future<Integer>> list = new ArrayList<>();
+        // Callable<Integer> callable = new PerStatsMulti(n);
+        for (int i = 0; i < t; i++) {
+            Future<Integer> future = executor.submit(new PerWorker(n));
+            list.add(future);
+        }
+        // for (int i = ...)
+        // fraction = list.stream().toArray(new double[]);
+        for (Future<Integer> fut : list) {
+            try {
+                fraction[index] = (double) fut.get() / (n * n);
+                index++;
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException();
             }
         }
-        return p.numberOfOpenSites();
+        executor.shutdown();
     }
 
-    private static double mean() {                             // sample mean of percolation threshold
+    private double mean() {                             // sample mean of percolation threshold
         return StdStats.mean(fraction);
     }
 
-    private static double stddev() {                           // sample standard deviation of percolation threshold
+    private double stddev() {                           // sample standard deviation of percolation threshold
         return StdStats.stddev(fraction);
     }
 
-    private static double confidenceLo() {
+    private double confidenceLo() {
         return StdStats.mean(fraction) - 1.96 * StdStats.stddev(fraction) /  Math.sqrt(fraction.length);
     }
 
-    private static double confidenceHi() {
+    private double confidenceHi() {
         return StdStats.mean(fraction) + 1.96 * StdStats.stddev(fraction) /  Math.sqrt(fraction.length);
     }
 
@@ -52,30 +83,15 @@ public class PerStatsMulti implements Callable<Integer> {
         int t = in.nextInt();
         int index = 0;
         fraction = new double[t];
-        ExecutorService executor = Executors.newFixedThreadPool(12);
-        ThreadPoolExecutor pool = (ThreadPoolExecutor) executor;
-        List<Future<Integer>> list = new ArrayList<>();
-        Callable<Integer> callable = new PerStatsMulti(n);
-        for (int i = 0; i < t; i++) {
-            Future<Integer> future = executor.submit(callable);
-            list.add(future);
-        }
-        for (Future<Integer> fut : list) {
-            try {
-                fraction[index] = (double) fut.get() / (n * n);
-                index++;
-            } catch (InterruptedException | ExecutionException e) {
-                throw new RuntimeException();
-            }
-        }
-        executor.shutdown();
+        PerStatsMulti p = PerStatsMulti(t);
+        p.calculate();
 
-        System.out.println("Current threads in pool = " + pool.getPoolSize());
-        System.out.println("Maximum allowed threads = " + pool.getMaximumPoolSize());
-        System.out.println("Total number of threads = " + pool.getTaskCount());
-        System.out.println("mean                    = " + mean());
-        System.out.println("stddev                  = " + stddev());
-        System.out.println("95% confidence interval = [" + confidenceLo() + ", " + confidenceHi() + "]");
+        System.out.println("Current threads in pool = " + p.getPoolSize());
+        System.out.println("Maximum allowed threads = " + p.getMaximumPoolSize());
+        System.out.println("Total number of threads = " + p.getTaskCount());
+        System.out.println("mean                    = " + p.mean());
+        System.out.println("stddev                  = " + p.stddev());
+        System.out.println("95% confidence interval = [" + p.confidenceLo() + ", " + p.confidenceHi() + "]");
         long stopTime = System.currentTimeMillis();
         long elapsedTime = stopTime - startTime;
         System.out.format("time                    = %.3f seconds (%.3f min)", elapsedTime / 1000.0, (double) elapsedTime/60000);
